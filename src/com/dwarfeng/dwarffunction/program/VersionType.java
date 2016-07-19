9e836029f33e6ed263d7a49f34559217fb2ed55f
DwArFeng <915724865@qq.com>
2016-07-19 23:02:51 +0800

    程序版本类型枚举实现

---
+++ src/com/dwarfeng/dwarffunction/program/VersionType.java
@@ -0,0 +1,32 @@
+package com.dwarfeng.dwarffunction.program;
+
+/**
+ * 编程中常见的版本类型。
+ * @author DwArFeng
+ * @since 1.8
+ */
+public enum VersionType {
+
+	/**内测版本*/
+	ALPHA("alpha"),
+	/**公测版本*/
+	BETA("beta"),
+	/**发布版本*/
+	RELEASE("release"),
+	/**预览版本*/
+	SNAPSHOTS("snapshots");
+	
+	private final String label;
+	
+	VersionType(String label){
+		this.label = label;
+	}
+	
+	/**
+	 * 返回版本的标签。
+	 * @return 版本的标签。
+	 */
+	public String getLabel(){
+		return this.label;
+	}
+}
