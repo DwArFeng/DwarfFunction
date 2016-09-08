653c2d89b08440b3b662af8bed2919cfb895ee67
skgy <skgy@skgy-PC>
2016-09-08 09:31:44 +0800

    Alpha_0.0.12_20160908_build_A
    Signed-off-by: skgy <skgy@skgy-PC>

--- src/com/dwarfeng/dfunc/gui/swing/PioltLight.java
+++ src/com/dwarfeng/dfunc/gui/swing/PioltLight.java
@@ -3,6 +3,8 @@ package com.dwarfeng.dfunc.gui.swing;
 import javax.swing.JPanel;
 
 class PioltLight extends JPanel {
+	private static final long serialVersionUID = 4054070447936368877L;
+
 	public PioltLight() {
 	}
 
