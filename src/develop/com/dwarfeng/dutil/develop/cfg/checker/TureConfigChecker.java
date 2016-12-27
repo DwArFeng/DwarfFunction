76f659d9d4e1fc0805bad5e0355482235f5f79ea
DwArFeng <915724865@qq.com>
2016-12-27 21:19:53 +0800

    20161227 Commit B
    Signed-off-by: DwArFeng <915724865@qq.com>

--- src/develop/com/dwarfeng/dutil/develop/cfg/checker/TureConfigChecker.java
+++ src/develop/com/dwarfeng/dutil/develop/cfg/checker/TureConfigChecker.java
@@ -3,13 +3,13 @@ package com.dwarfeng.dutil.develop.cfg.checker;
 import com.dwarfeng.dutil.develop.cfg.ConfigChecker;
 
 /**
- * 恒假配置值检查器。
- * <p> 该配置值检查器对于任意的值，均返回 <code>false</code>，换句话说，
- * 该配置值检查器拒绝一切值。
+ * 恒真式配置值检查器。
+ * <p> 该检查器对一个配置中的一切值的检查恒返回 <code>true</code>，换句话说，
+ * 该配置值检查器允许一切值。
  * @author  DwArFeng
  * @since 1.8
  */
-public class FalseConfigChecker implements ConfigChecker {
+public class TureConfigChecker implements ConfigChecker{
 
 	/*
 	 * (non-Javadoc)
@@ -17,7 +17,7 @@ public class FalseConfigChecker implements ConfigChecker {
 	 */
 	@Override
 	public boolean isValid(String value) {
-		return false;
+		return true;
 	}
 
 }
