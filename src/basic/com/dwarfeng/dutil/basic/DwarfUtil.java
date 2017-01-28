2a4ffecb7502cd42ddd7f8a3f79d20952a039758
DwArFeng <915724865@qq.com>
2017-01-28 10:52:57 +0800

    20170128 Commit D
    Signed-off-by: DwArFeng <915724865@qq.com>

--- src/basic/com/dwarfeng/dutil/basic/DwarfUtil.java
+++ src/basic/com/dwarfeng/dutil/basic/DwarfUtil.java
@@ -36,8 +36,8 @@ public final class DwarfUtil {
 	private static final String SF_PATH = "com/dwarfeng/dutil/resource/lang/stringField";
 	
 	private static final Version version = new DefaultVersion.Builder()
-			.type(VersionType.BETA).firstVersion((byte) 0).secondVersion((byte) 0).thirdVersion((byte) 2)
-			.buildDate("20170115").buildVersion('B')
+			.type(VersionType.BETA).firstVersion((byte) 0).secondVersion((byte) 0).thirdVersion((byte) 3)
+			.buildDate("20170128").buildVersion('A')
 			.build();
 	
 	private static ResourceBundle sf = ResourceBundle.getBundle(SF_PATH,Locale.getDefault(),CT.class.getClassLoader());
