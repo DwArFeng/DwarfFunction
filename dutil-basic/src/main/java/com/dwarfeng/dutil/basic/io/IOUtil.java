2e386a5fe1bf314af35ab1dd28003769cea8b98b
DwArFeng <915724865@qq.com>
2019-01-03 19:35:01 +0800

    : 优化 IOUtil 的 trans 方法
    Task-Url:
    
    Signed-off-by: DwArFeng <915724865@qq.com>

--- dutil-basic/src/main/java/com/dwarfeng/dutil/basic/io/IOUtil.java
+++ dutil-basic/src/main/java/com/dwarfeng/dutil/basic/io/IOUtil.java
@@ -1,7 +1,5 @@
 package com.dwarfeng.dutil.basic.io;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -17,29 +15,21 @@ public final class IOUtil {
 	/**
 	 * 数据传送方法。
 	 * <p>
-	 * 将一个输入流中的所有数据传递给一个输出流。
-	 * <p>
-	 * 该方法不会主动关闭 <code>in</code> 或者 <code>out</code>。
+	 * 将一个输入流中的所有数据传递给一个输出流。 <br>
+	 * 注意：该方法不会主动关闭 <code>in</code> 或者 <code>out</code>，需要调用额外的关闭方法关闭指定的输入流和输出流。
 	 * 
-	 * @param in
-	 *            指定的输入流。
-	 * @param out
-	 *            指定的输出流。
-	 * @param bufferSize
-	 *            指定的缓冲的大小。
-	 * @throws IOException
-	 *             当数据无法读取或数据无法写入时抛出的异常。
+	 * @param in         指定的输入流。
+	 * @param out        指定的输出流。
+	 * @param bufferSize 指定的缓冲的大小。
+	 * @throws IOException 当数据无法读取或数据无法写入时抛出的异常。
 	 */
 	public static void trans(InputStream in, OutputStream out, int bufferSize) throws IOException {
 		if (in == null || out == null)
 			throw new NullPointerException("InputStream or OutputStream can't be null");
-		BufferedInputStream bin = new BufferedInputStream(in, bufferSize);
-		BufferedOutputStream bout = new BufferedOutputStream(out, bufferSize);
 		byte[] buffer = new byte[bufferSize];
 		int i = 0;
-		while ((i = bin.read(buffer, 0, buffer.length)) >= 0) {
-			bout.write(buffer, 0, i);
-			bout.flush();
+		while ((i = in.read(buffer, 0, buffer.length)) >= 0) {
+			out.write(buffer, 0, i);
 		}
 	}
 
