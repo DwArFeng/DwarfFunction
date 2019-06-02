3dee8cfcc031e4b8e8b350147fbfe879fc7905e0
DwArFeng <915724865@qq.com>
2019-06-02 17:23:31 +0800

    : 删除部分过于耗时的测试方法
    Signed-off-by: DwArFeng <915724865@qq.com>

--- dutil-basic/src/test/java/com/dwarfeng/dutil/basic/cna/Test_ArrayUtil.java
+++ dutil-basic/src/test/java/com/dwarfeng/dutil/basic/cna/Test_ArrayUtil.java
@@ -9,9 +9,6 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import com.dwarfeng.dutil.basic.io.CT;
-import com.dwarfeng.dutil.basic.mea.TimeMeasurer;
-
 public class Test_ArrayUtil {
 
 	@BeforeClass
@@ -44,30 +41,6 @@ public class Test_ArrayUtil {
 				.getNonNull(new String[] { null, null, "1", null, null, "2", "3", null, "4", "5", null, null }));
 		assertArrayEquals(new String[] {}, (String[]) ArrayUtil.getNonNull(new String[] {}));
 		assertArrayEquals(new String[] {}, (String[]) ArrayUtil.getNonNull(new String[] { null, null, null }));
-	}
-
-	@Test
-	@SuppressWarnings("deprecation")
-	public final void getNonNullPerformanceCompare() {
-		final String[] testArray = new String[] { null, null, "1", null, null, "2", "3", null, "4", "5", null, null };
-		final int testTimes = 100000000;
-		final TimeMeasurer tm_1 = new TimeMeasurer();
-		final TimeMeasurer tm_2 = new TimeMeasurer();
-
-		tm_1.start();
-		for (int i = 0; i < testTimes; i++) {
-			ArrayUtil.getNotNull(testArray, new String[0]);
-		}
-		tm_1.stop();
-
-		tm_2.start();
-		for (int i = 0; i < testTimes; i++) {
-			ArrayUtil.getNonNull(testArray);
-		}
-		tm_2.stop();
-
-		CT.trace(String.format("旧方法使用时间：%d ms", tm_1.getTimeMs()));
-		CT.trace(String.format("新方法使用时间：%d ms", tm_2.getTimeMs()));
 	}
 
 }
